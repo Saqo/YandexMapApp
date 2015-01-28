418414ab0e8815c98dea66d21d3b3269a8605c8c
Saqo <saqoagadzhanyan@gmail.com>
2015-01-29 01:53:36 +0400

    v 1.0

---
+++ YandexMapApp/Controllers/YMController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YandexMapApp.Controllers
+{
+    public class YMController : Controller
+    {
+        //
+        // GET: /YM/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
